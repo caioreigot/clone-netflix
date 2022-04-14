9c6e8c5ca86759793c37497a48c9fbbc57e4ad84
caioreigot <caiocrrodrigues2004@gmail.com>
2022-04-14 13:00:16 -0300

    CSS incrementado para responsividade do dialog de MAIS INFORMAÇÕES

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-/*Evento disparado quando a pagina está pronta para apresentação*/
+/*//Evento disparado quando a pagina está pronta para apresentação
 window.onload = () => {
-    /*Atribuindo um botão do HTML para uma variável global*/
-    rightButton = document.getElementsByClassName("botao")[1];
+    //Atribuindo um botão do HTML para uma variável global
+    rightButton = document.getElementsByClassName("botao")[2];
     checkMarginNeed();
 }
 
-/*Evento disparado quando o document view é redimensionado*/
+//Evento disparado quando o document view é redimensionado
 window.addEventListener('resize', () => {
     checkMarginNeed();
 })
+*/
 
+/*
 function checkMarginNeed() {
-    /*Atribuindo o clientWidth de uma div de classe "container" para uma variável local*/
+    //Atribuindo o clientWidth de uma div de classe "container" para uma variável local
     let containerWidth = document
         .getElementsByClassName("filme-principal")[0]
         .getElementsByClassName("container")[0].clientWidth;
 
-    /*Atribuindo um valor para a margem superior do botão "mais informações" 
-    dependendo da largura do container à qual este botão está aninhado*/
+    //Atribuindo um valor para a margem superior do botão "mais informações" 
+    //dependendo da largura do container à qual este botão está aninhado
     rightButton.style.marginTop = (containerWidth <= 443) ? '8px' : '0px';
 }
+*/
 
 /*Implementando o Owl Carousel com o JQuery*/
 $('.owl-carousel').owlCarousel({
