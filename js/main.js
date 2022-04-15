8cc9b5ff31ee0fee41acf14e2b14aaa82b839a0e
caioreigot <caiocrrodrigues2004@gmail.com>
2022-04-15 03:00:14 -0300

    Código comentado removido & adicionado comentários no CSS do carousel

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-/*//Evento disparado quando a pagina está pronta para apresentação
-window.onload = () => {
-    //Atribuindo um botão do HTML para uma variável global
-    rightButton = document.getElementsByClassName("botao")[2];
-    checkMarginNeed();
-}
-
-//Evento disparado quando o document view é redimensionado
-window.addEventListener('resize', () => {
-    checkMarginNeed();
-})
-*/
-
-/*
-function checkMarginNeed() {
-    //Atribuindo o clientWidth de uma div de classe "container" para uma variável local
-    let containerWidth = document
-        .getElementsByClassName("filme-principal")[0]
-        .getElementsByClassName("container")[0].clientWidth;
-
-    //Atribuindo um valor para a margem superior do botão "mais informações" 
-    //dependendo da largura do container à qual este botão está aninhado
-    rightButton.style.marginTop = (containerWidth <= 443) ? '8px' : '0px';
-}
-*/
-
 /*Implementando o Owl Carousel com o JQuery*/
 $('.owl-carousel').owlCarousel({
     loop: true,
